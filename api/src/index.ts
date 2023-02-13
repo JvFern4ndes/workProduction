a04e91f7b3e5fd5413c6e34faae7a6bad1f27466
João Victor <jvfernandes.contato@gmail.com>
2023-02-13 07:10:38 -0300

    feat: conditioning the operation of the server to the connection with mongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const app = express();
-
 mongoose.set('strictQuery', true);
 
 mongoose.connect('mongodb://localhost:27017')
-  .then(() => console.log('Conectado ao mongoDB'))
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(3001, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
+  })
   .catch(() => console.log('Erro ao conectar ao mongoDB'));
 
-const port = 3001;
-app.listen(3001, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
-});
+
