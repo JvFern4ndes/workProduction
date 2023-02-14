2799bf417fe14aeeea753fa3b7205f78799f8fdd
João Victor <jvfernandes.contato@gmail.com>
2023-02-14 00:34:29 -0300

    refactor: refactoring the create employee

--- api/src/app/useCases/Employees/createEmployee.ts
+++ api/src/app/useCases/Employees/createEmployee.ts
@@ -3,9 +3,30 @@ import { Request, Response } from 'express';
 import { Employee } from '../../models/Employee';
 
 export async function createEmployees(req: Request, res: Response) {
-  const { name, userName, password } = req.body;
+  try {
+    const {
+      name,
+      email,
+      phone,
+      imagePath,
+      userName,
+      password,
+      period
+    } = req.body;
 
-  const employee = await Employee.create({ name, userName, password });
+    const employee = await Employee.create({
+      name,
+      email,
+      phone,
+      imagePath,
+      userName,
+      password,
+      period
+    });
 
-  res.json(employee);
+    res.status(201).json(employee);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
