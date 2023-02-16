281187e673b41b2b370757bc7303a8217e830cdb
João Victor <jvfernandes.contato@gmail.com>
2023-02-16 14:51:49 -0300

    feat: starting mobile using expo

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles';
 const orders: Order[] = [
   {
     '_id': '63eb80fc7868e22b1fc669d0',
-    'status': 'waiting',
+    'status': 'in_production',
     'client': {
       '_id': '63eb5d551a09515da6f09a82',
       'name': 'Safecar',
