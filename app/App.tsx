96e8f9caba07dd517f177419b092a29f6b13a4bf
João Victor <jvfernandes.contato@gmail.com>
2023-03-14 23:36:10 -0300

    feat: reconfiguring the react navigation

--- app/App.tsx
+++ app/App.tsx
@@ -2,7 +2,7 @@ import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 import { Login } from './src/Login';
@@ -10,7 +10,7 @@ import { Main } from './src/Main';
 
 import { AuthContext } from './src/components/Context/authContext';
 
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
@@ -26,11 +26,11 @@ export default function App() {
   return (
     <AuthContext>
       <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name='StatusBar' component={StatusBar}/>
-          <Drawer.Screen name='Login' component={Login}/>
-          <Drawer.Screen name='Main' component={Main}/>
-        </Drawer.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name='StatusBar' component={StatusBar}/>
+          <Stack.Screen name='Login' component={Login}/>
+          <Stack.Screen name='Main' component={Main}/>
+        </Stack.Navigator>
       </NavigationContainer>
     </AuthContext>
   );
